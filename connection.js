a607ce39ba3da4ab506e5c1db0dd6f7adaf4c252
Ayush porwal <95545423+ayushporwal7078@users.noreply.github.com>
2023-03-02 19:53:11 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://localhost:27017/', ()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/?', ()=>{
     console.log("connected to mongodb")
 })
 
