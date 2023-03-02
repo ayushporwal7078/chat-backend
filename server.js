6b83b86efbd52366ed5a432e48d2c1ccc4114e62
ayushporwal7078 <ayushporwal7078@gmail.com>
2023-03-02 19:37:37 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const userRoutes = require('./routes/userRoutes') 
+const rooms = ['general', 'tech', 'finance', 'crypto']
+
+const cors = require('cors')
+
+app.use(express.urlencoded({extended : true}))
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/users', userRoutes)
+require('./connection')
+
+const server = require('http').createServer(app);
+const PORT = 5001;
+
+// const io = require('socket.io')(server, {
+//     cors: {
+//         origin: "http://localhost:3000",
+//         METHODS : ['GET','POST']
+//     }
+// })
+
+server.listen(PORT,()=>{
+    console.log("listening on port http://localhost:5001")
+})
+
